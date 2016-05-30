046e98e34cb2408a1c1535ba2f2099c897d0bc14
rajus74 <rshah@veriskhealth.com>
2016-05-30 22:05:15 +0545

    Added files for Guess the Number

---
+++ Slidify Pitch of Guess The Number/codes.R
@@ -0,0 +1,11 @@
+#install and load devtools
+install.packages("devtools")
+library(devtools)
+
+#install and load Slidify
+install_github('slidify', 'rajus74')
+install_github('slidifyLibraries', 'rajus74')
+library(slidify)
+
+author("Guess_The_Number")
+
